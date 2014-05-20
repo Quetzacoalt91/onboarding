ed692d44f7b50b6ebfd1b2c7708a46642db2aefe
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-05-20 07:09:59 +0200

    //Starting PrestaShop OnBoarding module

---
+++ onboarding.php
@@ -0,0 +1,50 @@
+<?php
+if (!defined('_PS_VERSION_'))
+	exit;
+
+class OnBoarding extends Module
+{
+	public function __construct()
+	{
+		$this->name = 'onboarding';
+        $this->bootstrap = true;
+		$this->tab = 'administration';
+		$this->version = '0.1';
+		$this->author = 'PrestaShop';
+		$this->displayName = $this->l('OnBoarding');
+		$this->description = $this->l('PrestaShop OnBoarding');
+
+		parent::__construct();
+	}
+
+	public function install()
+	{
+		if (parent::install() && $this->registerHook('displayBackOfficeHeader')
+			&& $this->registerHook('displayBackOfficeTop'))
+			return true;
+
+		return false;
+	}
+	
+	public function uninstall()
+	{
+		if (!parent::uninstall())
+			return false;
+
+		return true;
+	}
+
+	public function hookDisplayBackOfficeHeader()
+	{
+		$this->context->controller->addCSS($this->_path.'css/onboarding.css');
+	}
+
+	public function hookDisplayBackOfficeTop()
+	{
+		$this->context->smarty->assign(array(
+			'employee' => $this->context->employee,
+		));
+
+		return $this->display(__FILE__, 'backoffice_top.tpl');
+	}
+}
---
+++ views/templates/hook/backoffice_top.tpl
@@ -0,0 +1,11 @@
+<div class="onboarding">
+	<div class="overlay">
+		<div class="panel">
+			<div class="row">
+				<div class="col-xs-12">
+					<h3 class="text-center">{l s='Welcome on your PrestaShop shop, %s' sprintf=[$employee->firstname] mod='onboarding'}</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
